764c40349d060da6a207e415a6d69c36210336fd
Masashi Hashimoto <mhashimo200@gmail.com>
2019-12-17 15:29:02 +0900

    upper computed

--- js/watcher.js
+++ js/watcher.js
@@ -2,19 +2,30 @@ new Vue({
     el: '#app',
     data: {
         name: '',
-        upperName: ''
+        //upperName: ''
     },
+    /*
     created: function(){
         this.delayFunc = _.debounce(this.getUpper, 2000);
     },
+    */
+    /*
     watch: {
         name: function(newValue, oldValue){
             this.delayFunc();
         }
     },
+    */
+    computed: {
+        upperName: function(){
+            return this.name.toUpperCase();
+        }
+    },
+    /*
     methods: {
         getUpper: function(){
             this.upperName = this.name.toUpperCase();
         }
     }
+    */
 });
