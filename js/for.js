6ea88d0a191601a4112ab3eaf95afdeb4199328b
Masashi Hashimoto <mhashimo200@gmail.com>
2019-12-25 22:48:33 +0900

    3-50,51

---
+++ js/for.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: "#app",
+    data: {
+        books: [
+            {
+                isbn: "11111111",
+                title: "test title11",
+                price: 2000
+            },
+            {
+                isbn: "22222222",
+                title: "test 2222",
+                price: 2200
+            },
+            {
+                isbn: "3333",
+                title: "test title33",
+                price: 2300
+            }
+        ]
+    }
+})
