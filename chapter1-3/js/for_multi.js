21aca5c7d768fde0499009f9c06702abdae53b02
Masashi Hashimoto <mhashimo200@gmail.com>
2019-12-28 11:34:47 +0900

    3-60,61

---
+++ chapter1-3/js/for_multi.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data: {
+        songs: [
+            {
+                title: '赤トンボ',
+                lyrics: '夕焼け小焼の赤トンボ',
+                composer: '山田耕作'
+            },
+            {
+                title: 'クロトンボ',
+                lyrics: '夕焼け小焼のくろトンボ',
+                composer: '山田クロ作'
+            }     
+        ]
+    }
+});
