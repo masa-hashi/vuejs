ef4f75daf861a0fe9fd8eeec3e90f3a6f1c10be0
Masashi Hashimoto <mhashimo200@gmail.com>
2019-12-28 11:13:32 +0900

    3-53,54

---
+++ js/for_obj.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        book: {
+            isbn: '978-4-0988-9898-1',
+            title: 'JavaScript逆引き',
+            price: 2800
+        }
+    }
+});
