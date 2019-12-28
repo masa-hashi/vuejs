301b43ebea41e7a52bdbbda961fcab9b358ae9f7
Masashi Hashimoto <mhashimo200@gmail.com>
2019-12-28 11:26:28 +0900

    3-57,58

---
+++ chapter1-3/js/for_filter.js
@@ -0,0 +1,29 @@
+new Vue({
+    el: "#app",
+    data: {
+        books: [
+            {
+                isbn: "11111111",
+                title: "test title11",
+                price: 2000
+            },
+            {
+                isbn: "22222222",
+                title: "test 2222",
+                price: 2200
+            },
+            {
+                isbn: "3333",
+                title: "test title33",
+                price: 2300
+            }
+        ]
+    },
+    computed: {
+        expensiveBooks: function(){
+            return this.books.filter(function(b){
+                return b.price >= 2200;
+            })
+        }
+    }
+});
