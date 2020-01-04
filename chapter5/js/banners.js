243edae8e5ab060386f7dadbdaea7e019dd1ab55
Masashi Hashimoto <mhashimo200@gmail.com>
2020-01-04 22:32:50 +0900

    5-1,2,3

--- chapter5/js/banners.js
+++ chapter5/js/banners.js
@@ -1,27 +1,27 @@
 Vue.component('banner-member', {
-    template: 
-        `<div class="banner">
-            <h3>WINGSメンバー募集中</h3>
-            <p>あなたも参加しませんか？</P>
-        </div>`
-});
-
-Vue.component('banner-new', {
-    template: 
-        `<div class="banner">
-            <h3>WINGSメンバー募集中new</h3>
-            <p>あなたも参加しませんか？new</P>
-        </div>`
-});
-
-Vue.component('banner-env', {
-    template: 
-        `<div class="banner">
-            <h3>WINGSメンバー募集中env</h3>
-            <p>あなたも参加しませんか？env</P>
-        </div>`
-});
-
+    template: `<div class="banner">
+      <h3>WINGSメンバー募集中！</h3>
+      <p>あなたもWINGSプロジェクトに参加しませんか？<br />
+        現在、WINGSプロジェクトでは、ご一緒にお仕事ができる仲間を募集中です。</p>
+    </div>`
+  });
+  
+  Vue.component('banner-new', {
+    template: `<div class="banner">
+      <h3>新刊「HTML5超入門 」発売</h3>
+      <p>ステップバイステップで学ぶ入門書です。<br />
+        HTML5アプリの基礎知識、画面のデザイン、コードの書き方などが理解できます。</p>
+    </div>`
+  });
+  
+  Vue.component('banner-env', {
+    template: `<div class="banner">
+      <h3>環境構築設定</h3>
+      <p>開発環境の設定方法を図を交えて詳しく解説します。<br />
+        紹介している各モジュールは、日々頻繁にバージョンアップが行われています。</p>
+    </div>`
+  });
+  
 new Vue({
     el: '#app',
     created: function() {
